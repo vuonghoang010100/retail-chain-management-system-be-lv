0259d1a8f9a3e7d4760c78da1861b463d75b393a
Vuong Hoang <vuonghoang010100@gmail.com>
2024-08-05 15:12:46 +0700

    feat: change build local refer to issue [BE] #8

--- build.ps1
+++ build.ps1
@@ -5,3 +5,6 @@ docker build -t retail-system-be-java .
 docker image tag retail-system-be-java vuonghoang010100/retail-system:be-2.0-java
 
 docker push vuonghoang010100/retail-system:be-2.0-java
+
+docker rmi retail-system-be-java
+docker rmi vuonghoang010100/retail-system:be-2.0-java
